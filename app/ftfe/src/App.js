9b0a0f77a8f61a5272c8c27615f93987fcccf33e
Tom郭俊杰 <1501832474@qq.com>
2018-10-22 15:43:53 +0800

    Fix naming.

--- app/ftfe/src/App.js
+++ app/ftfe/src/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import {Route, BrowserRouter} from 'react-router-dom'
-import TopHeader from './topHeader/topHeader.js'
+import TopHeader from './top_header/top_header.js'
 import Home from './home/home.js'
 import Search from './search/search.js'
-import BottomFooter from './bottomFooter/bottomFooter.js'
+import BottomFooter from './bottom_footer/bottom_footer.js'
 
 class App extends Component {
   render() {
