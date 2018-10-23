48d933be0557b17087166bd088eb2c38c4c0b5ee
runninggoat <1501832474@qq.com>
2018-10-23 23:15:50 +0800

    Complete login feature.

--- app/ftfe/src/top_header/login.js
+++ app/ftfe/src/top_header/login.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
-import { Row, Col, Icon } from 'antd'
+import { Row, Col, Icon, Input, Button } from 'antd'
 
 export default class LoginPanel extends Component {
   render () {
     return (
-      <div>
+      <div style={{
+        position: 'fixed',
+        top: 0,
+        width: '100%',
+        height: '100%',
+        background: 'rgba(45, 46, 45, 0.3)',
+      }}>
         <Row
           type="flex"
           justify="center"
@@ -23,21 +29,70 @@ export default class LoginPanel extends Component {
               background: '#212221',
               borderRadius: '10px',
             }}>
-              <div
-                onClick={ () => this.props.onCloseLogin('Close') }
-                style={{
-                  color: 'white',
-                  fontSize: '30px',
-                  float: 'right',
-                  marginRight: '6px',
-                }}
-              >
-                <Icon type="close" theme="outlined" />
-              </div>
+              <Row>
+                <Col span={24}>
+                  <div
+                    onClick={ () => this.props.onCloseLogin('Close') }
+                    style={{
+                      color: 'white',
+                      fontSize: '30px',
+                      float: 'right',
+                      marginRight: '6px',
+                      textAlign: 'center',
+                    }}
+                  >
+                    <Icon type="close" theme="outlined" />
+                  </div>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center">
+                <Col span={8}>
+                  <div style={{
+                    color: 'white',
+                    fontSize: '30px',
+                    fontWeight: 700,
+                  }}>LOGIN</div>
+                </Col>
+              </Row>
+              <LoginForm />
             </div>
           </Col>
         </Row>
       </div>
+    )
+  }
+}
+
+class LoginForm extends Component {
+  render () {
+    return (
+      <Row>
+        <Row style={{ marginTop: '20px' }}>
+          <Col span={16} offset={4}>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.35)' }} />}
+              placeholder="User Name"
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginTop: '20px' }}>
+          <Col span={16} offset={4}>
+            <Input
+              type="password"
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.35)' }} />}
+              placeholder="Password"
+            />
+          </Col>
+        </Row>
+        <Row style={{ marginTop: '20px' }}>
+          <Col span={16} offset={4}>
+            <Button type="primary" style={{
+              color: 'black',
+              width: '100%',
+            }}>LOGIN</Button>
+          </Col>
+        </Row>
+      </Row>
     )
   }
 }
