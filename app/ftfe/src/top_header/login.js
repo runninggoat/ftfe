1492e2d7ba15c89ab5bd97c90eb9fbff3d727187
Tom郭俊杰 <1501832474@qq.com>
2018-10-24 14:37:47 +0800

    Add sign up feature. Complete login dialog feature.

--- app/ftfe/src/top_header/login.js
+++ app/ftfe/src/top_header/login.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react'
-import { Row, Col, Icon, Input, Button } from 'antd'
+import { Row, Col, Icon, Input, Button, Tabs } from 'antd'
+
+const TabPane = Tabs.TabPane
 
 export default class LoginPanel extends Component {
+
+  handleLogin () {
+    this.props.onCloseLogin('Login')
+  }
+
+  handleSignup () {
+    this.props.onCloseLogin('Signup')
+  }
+
   render () {
     return (
       <div style={{
@@ -22,10 +33,9 @@ export default class LoginPanel extends Component {
             width: '100%',
           }}
         >
-          <Col span={6}>
+          <Col span={5}>
             <div style={{
               width: '100%',
-              height: '300px',
               background: '#212221',
               borderRadius: '10px',
             }}>
@@ -46,15 +56,21 @@ export default class LoginPanel extends Component {
                 </Col>
               </Row>
               <Row type="flex" justify="center">
-                <Col span={8}>
+                <Col span={12}>
                   <div style={{
+                    marginTop: '-20px',
                     color: 'white',
                     fontSize: '30px',
                     fontWeight: 700,
-                  }}>LOGIN</div>
+                    textAlign: 'center',
+                  }}>FT 链</div>
                 </Col>
               </Row>
-              <LoginForm />
+              <LoginForm
+                onLogin={ this.handleLogin.bind(this) }
+                onSignup={ this.handleSignup.bind(this) }
+              />
+              <div style={{ clear: 'both' }}></div>
             </div>
           </Col>
         </Row>
@@ -63,35 +79,130 @@ export default class LoginPanel extends Component {
   }
 }
 
-class LoginForm extends Component {
+class TabTitle extends Component {
   render () {
     return (
-      <Row>
-        <Row style={{ marginTop: '20px' }}>
-          <Col span={16} offset={4}>
-            <Input
-              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.35)' }} />}
-              placeholder="User Name"
-            />
-          </Col>
-        </Row>
-        <Row style={{ marginTop: '20px' }}>
-          <Col span={16} offset={4}>
-            <Input
-              type="password"
-              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.35)' }} />}
-              placeholder="Password"
-            />
-          </Col>
-        </Row>
-        <Row style={{ marginTop: '20px' }}>
-          <Col span={16} offset={4}>
-            <Button type="primary" style={{
-              color: 'black',
-              width: '100%',
-            }}>LOGIN</Button>
-          </Col>
-        </Row>
+      <div style={{
+        color: this.props.active ? '' : 'white',
+      }}>
+        { this.props.text }
+      </div>
+    )
+  }
+}
+
+class LoginForm extends Component {
+  state = {
+    activeKey: 1,
+    visual: false,
+  }
+
+  handleChange (k) {
+    this.setState((state) => {
+      return { activeKey: parseInt(k) }
+    })
+  }
+
+  toggleVisual () {
+    this.setState((state) => {
+      return { visual: !state.visual }
+    })
+  }
+
+  render () {
+    return (
+      <Row type="flex" justify="center">
+        <Col span={18}>
+          <Tabs
+            onChange={ this.handleChange.bind(this) }
+            defaultActiveKey="1"
+            size="large"
+            tabBarStyle={{ border: 0, textAlign:'center' }}
+          >
+            <TabPane tab={ <TabTitle text="登录" active={ this.state.activeKey === 1 } /> } key="1">
+              <Row type="flex" justify="center" style={{ marginTop: '20px' }}>
+                <Col span={24}>
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.35)' }} />}
+                    placeholder="邮箱/手机号码"
+                  />
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" style={{ marginTop: '20px' }}>
+                <Col span={24}>
+                  <Input
+                    type={ this.state.visual ? 'text' : 'password' }
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.35)' }} />}
+                    suffix={<Icon type="eye" style={{ color: 'rgba(0,0,0,.35)' }} onClick={() => this.toggleVisual()} />}
+                    placeholder="密码"
+                  />
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" style={{ marginTop: '20px' }}>
+                <Col span={24}>
+                  <Button
+                    onClick={ () => this.props.onLogin() }
+                    type="primary"
+                    style={{
+                      color: 'black',
+                      width: '100%',
+                    }}
+                  >登录</Button>
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" style={{ marginTop: '20px', marginBottom: '20px' }}>
+                <Col span={24}>
+                  <p style={{
+                    color: 'white',
+                    textAlign: 'center',
+                    textDecoration: 'underline',
+                    fontSize: '16px',
+                  }}>忘记密码？</p>
+                </Col>
+              </Row>
+            </TabPane>
+            <TabPane tab={ <TabTitle text="注册" active={ this.state.activeKey === 2 } /> } key="2">
+              <Row type="flex" justify="center" style={{ marginTop: '20px' }}>
+                <Col span={24}>
+                  <Input
+                    prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.35)' }} />}
+                    placeholder="邮箱/手机号码"
+                  />
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" style={{ marginTop: '20px' }}>
+                <Col span={24}>
+                  <Input
+                    type="password"
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.35)' }} />}
+                    placeholder="设置密码"
+                  />
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" style={{ marginTop: '20px' }}>
+                <Col span={24}>
+                  <Input
+                    type="password"
+                    prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.35)' }} />}
+                    placeholder="重复确认密码"
+                  />
+                </Col>
+              </Row>
+              <Row type="flex" justify="center" style={{ marginTop: '20px', marginBottom: '40px' }}>
+                <Col span={24}>
+                  <Button
+                    onClick={ () => this.props.onSignup() }
+                    type="primary"
+                    style={{
+                      color: 'black',
+                      width: '100%',
+                    }}
+                  >注册</Button>
+                </Col>
+              </Row>
+            </TabPane>
+          </Tabs>
+        </Col>
       </Row>
     )
   }
