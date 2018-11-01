a19a8d585389e1cd4ae571f55b0a34185c38a123
Nanqing Li <lnqjacky@gmail.com>
2018-11-01 04:37:28 +0000

    hot movie ui

--- app/ftfe/src/home/hot_movie_cards.js
+++ app/ftfe/src/home/hot_movie_cards.js
@@ -22,16 +22,17 @@ class HotMovieCard extends Component{
         borderBottomLeftRadius:'16px',borderBottomRightRadius:'16px',
       }}> 
         <div>
-          <Tag style={{position:'absolute',top:'5%',left:'5%',background:"linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)"}}>
-            <span style={{color:'#FFFFFF',borderRadius:'10px'}}>{this.props.type1?this.props.type1:'音乐'}</span> 
+          <Tag style={{position:'absolute',top:'5%',left:'5%',borderRadius:'10px',background:"linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)"}}>
+            <span style={{color:'#FFFFFF'}}>{this.props.type1?this.props.type1:'音乐'}</span> 
           </Tag>
       </div>
+      
         <div style={{fontWight:'900',color:'rgba(0,0,0,1)',padding:'2%',
-        }}>{this.props.title?this.props.title:'自媒体《独白》团队访问徐峥：药神不是药神,徐峥还是徐峥?'}
-          <div>{this.props.title2?this.props.title2:''}</div>
+        }}>{this.props.title1?this.props.title1:'自媒体《独白》团队访问徐峥：药神不是药神,徐峥还是徐峥?'}
+          <div style={{color:'rgba(155,155,155,1)'}}>{this.props.title2?this.props.title2:''}</div>
         </div>
-        <div style={{color:'#9B9B9B',padding:'2%',}}>
-          <Tag color="#EEEEEE"><span style={{color:'#9B9B9B',borderRadius:'10px'}}>{this.props.type2?this.props.type2:'言情'}</span> </Tag>
+        <div style={{color:'#9B9B9B',padding:'2%'}}>
+          <Tag color="#EEEEEE" style={{borderRadius:'10px'}}><span style={{color:'#9B9B9B'}}>{this.props.type2?this.props.type2:'言情'}</span> </Tag>
           <span style={{float:'right'}}>X 9,999   X 10万</span>
         </div>
       </div>
--- app/ftfe/src/home/hot_movie_list.js
+++ app/ftfe/src/home/hot_movie_list.js
@@ -2,7 +2,19 @@ import React from 'react'
 import HotMovieCard from './hot_movie_cards'
 
 import { Row, Col } from 'antd';
-import img1 from'../image/006.jpg'
+
+import img1 from'../image/001.jpeg'
+import img3 from'../image/003.jpg'
+import img4 from'../image/004.jpeg'
+import img5 from'../image/005.jpg'
+import img6 from'../image/006.jpg'
+import img7 from'../image/007.jpg'
+import img8 from'../image/008.jpg'
+import img9 from'../image/009.jpg'
+import img10 from'../image/010.jpg'
+import img11 from'../image/011.jpg'
+import img12 from'../image/012.jpg'
+import img13 from'../image/013.jpg'
   
   
 export default () => (
@@ -39,15 +51,21 @@ export default () => (
                     <Row>
                         <Col span={1} />
                         <Col span={6} ><HotMovieCard 
-                        img={img1} title1={"自媒体《独白》团队访问徐峥：药神不是药神,徐峥还是徐峥"}
-                        type2={'访谈'} type1={'视频'}
+                        img={img6} title1={"自媒体《独白》团队访问徐峥：药神不是药神,徐峥还是徐峥"}
+                        type1={'视频'} type2={'访谈'} 
                         /></Col>
-                        <Col span={5} ><HotMovieCard /></Col>
                         <Col span={5} ><HotMovieCard 
-                        img={img1} title1={"自媒体《独白》团队访问徐峥：药神不是药神,徐峥还是徐峥"}
-                        title2={'Mumford & Sons'} type2={'独立摇滚'}
+                        img={img4} title1={"穿越清朝智斗妃嫔成皇后一统后宫"}
+                        type1={'文学'} type2={'言情'} 
                         /></Col>
-                        <Col span={6} ><HotMovieCard /></Col>
+                        <Col span={5} ><HotMovieCard 
+                        img={img13} title1={"BABEL"}
+                        title2={'Mumford & Sons'} type1={'音乐'} type2={'独立摇滚'}
+                        /></Col>
+                        <Col span={6} ><HotMovieCard 
+                        img={img1} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
+                        type1={'其他'} type2={'编舞'} 
+                        /></Col>
                         <Col span={1} />
                     </Row>
                 </Col>
@@ -56,10 +74,22 @@ export default () => (
                 <Col span={24} >
                     <Row>
                         <Col span={1} />
-                        <Col span={5} ><HotMovieCard /></Col>
-                        <Col span={6} ><HotMovieCard /></Col>
-                        <Col span={6} ><HotMovieCard /></Col>
-                        <Col span={5} ><HotMovieCard /></Col>
+                        <Col span={5} ><HotMovieCard 
+                        img={img7} title1={"穿越清朝智斗妃嫔成皇后一统后宫"}
+                        type1={'文学'} type2={'言情'} 
+                        /></Col>
+                        <Col span={6} ><HotMovieCard 
+                        img={img12} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
+                        type1={'其他'} type2={'访谈'} 
+                        /></Col>
+                        <Col span={6} ><HotMovieCard 
+                        img={img9} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
+                        type1={'视频'} type2={'访谈'} 
+                        /></Col>
+                        <Col span={5} ><HotMovieCard 
+                        img={img10} title1={"BABEL"}
+                        title2={'Mumford & Sons'} type1={'音乐'} type2={'独立摇滚'}
+                        /></Col>
                         <Col span={1} />
                     </Row>
                 </Col>
