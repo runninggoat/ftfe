25beda4f5bed7c54bc67656e02d5805dc9279972
Nanqing Li <lnqjacky@gmail.com>
2018-11-05 03:10:56 +0000

    watchlist

--- app/ftfe/src/home/watch_list.js
+++ app/ftfe/src/home/watch_list.js
@@ -37,10 +37,10 @@ export default () => (
                                     style={{padding:'1%',background:'linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)'}}
                                 />
                                 <span style={{fontWeight:600,fontSize:'150%',color:'#222222',}}>
-                                    &nbsp;&nbsp;热门佳作
+                                    &nbsp;&nbsp;我的关注
                                 </span>
                                 <span style={{fontWeight:600,fontSize:'150%',color:'#BCBCBC'}}>
-                                    &nbsp;&nbsp;POPULAR
+                                    &nbsp;&nbsp;FAVOURATE
                                 </span>
                                 <span style={{fontWeight:400,fontSize:'100%',color:'#BCBCBC'}}>
                                     &nbsp;&nbsp;查看更多 》
@@ -53,41 +53,41 @@ export default () => (
                             <Row gutter={12}>
                                 <Col span={7} >
                                     <MovieCards 
-                                    img={img6} title1={"自媒体《独白》团队访问徐峥：药神不是药神,徐峥还是徐峥"}
+                                    img={img13} title1={"自媒体《独白》团队访问徐峥：药神不是药神,徐峥还是徐峥"}
                                     type1={'视频'} type2={'访谈'} time={'1460:59'}
                                     /></Col>
                                     <Col span={4} ><MovieCards 
-                                    img={img4} title1={"穿越清朝智斗妃嫔成皇后一统后宫"}
+                                    img={img10} title1={"穿越清朝智斗妃嫔成皇后一统后宫"}
                                     type1={'文学'} type2={'言情'} time={'长篇小说'}
                                     /></Col>
                                     <Col span={6} ><MovieCards 
-                                    img={img13} title1={"BABEL"}
+                                    img={img7} title1={"BABEL"}
                                     title2={'Mumford & Sons'} type1={'音乐'} type2={'独立摇滚'} time={'10:59'}
                                     /></Col>
                                     <Col span={7} ><MovieCards 
-                                    img={img1} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
+                                    img={img4} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
                                     type1={'其他'} type2={'编舞'} time={'1460:59'}
                                     /></Col>
                             </Row></div>
                         </Col>
                     <Row>
                     </Row>
-                        <Col span={24}><div style={{margin:'1% 0 3%'}}>
+                        <Col span={24}><div style={{margin:'1% 0 1%'}}>
                             <Row gutter={12}>
                                 <Col span={4} ><MovieCards 
-                                    img={img7} title1={"穿越清朝智斗妃嫔成皇后一统后宫"}
+                                    img={img12} title1={"穿越清朝智斗妃嫔成皇后一统后宫"}
                                     type1={'文学'} type2={'言情'} time={'长篇小说'}
                                 /></Col>
                                 <Col span={7} ><MovieCards 
-                                    img={img12} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
+                                    img={img9} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
                                     type1={'其他'} type2={'访谈'} time={'1460:59'}
                                 /></Col>
                                 <Col span={7} ><MovieCards
-                                    img={img9} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
+                                    img={img1} title1={"自媒体《独白》团队访问徐峥：药神不是药神，徐峥还是徐峥"}
                                     type1={'视频'} type2={'访谈'} time={'1460:59'}
                                 /></Col>
                                 <Col span={6} ><MovieCards 
-                                    img={img10} title1={"BABEL"} time={'10:59'}
+                                    img={img5} title1={"BABEL"} time={'10:59'}
                                     title2={'Mumford & Sons'} type1={'音乐'} type2={'独立摇滚'}
                                 /></Col>
                             </Row></div>
