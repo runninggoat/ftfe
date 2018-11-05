dabb0e570e497b184f3737abb29d4890ca7d0545
Nanqing Li <lnqjacky@gmail.com>
2018-11-05 08:00:03 +0000

    comment editor

--- app/ftfe/src/modal/comment/comment_editor.js
+++ app/ftfe/src/modal/comment/comment_editor.js
@@ -2,21 +2,21 @@ import React from 'react';
 import CommentEditor from './comment_editor';
 import CommentComments from './comment_comments';
 import { Row, Col } from 'antd';
-
+import MyIcon from '../../my_icon'
 
 import { Input,Icon,Button } from 'antd';
 const { TextArea } = Input;
 
 
 export default () => (
-  <div style={{padding:'2.5%', margin:'1% 1% 1% 1%',backgroundColor:'purple',borderRadius:'16px'}}>
+  <div style={{padding:'2.5%', margin:'1% 1% 1% 1%',backgroundColor:'#FFFFFF',borderRadius:'16px'}}>
     
     <Row>
       <Col>
         <Row>
           <Col>
             <div style={{ margin: '1% 0' }}>
-              <TextArea  placeholder="Autosize height based on content lines" autosize={{ minRows: 3, maxRows: 3 }} />
+              <TextArea  placeholder="在此写下您的评论 …" autosize={{ minRows: 3, maxRows: 3 }} />
             </div>
           </Col>
         </Row>
@@ -25,20 +25,30 @@ export default () => (
           <Col span={3}>
             <Row>
               <Col span={8}>
-                <Icon type="smile" theme="filled" style={{fontSize:'150%',marginLeft:'1%',marginRight: '1%'}}/>
+                <MyIcon type="icon-picture"  style={{fontSize:'150%',marginLeft:'1%',marginRight: '1%'}}/>
               </Col >
               <Col span={8}>
-                <Icon type="tag" theme="outlined" style={{fontSize:'150%',marginRight: '1%'}}/>
+                <MyIcon type="icon-picture"  style={{fontSize:'150%',marginRight: '1%'}}/>
               </Col>
               <Col span={8}>
-                <Icon type="diff" theme="filled" style={{fontSize:'150%',marginRight: '1%'}} />
+                <MyIcon type="icon-picture" style={{fontSize:'150%',marginRight: '1%'}} />
               </Col>
             </Row>
           </Col>
           
-          <Col span={20} />
-          <Col span={1}>
-            <Button size={'small'} style={{fontSize:'15px',float: 'right'}} >发表</Button>
+          <Col span={19} />
+          <Col span={2}>
+            <div>
+              <Button style={{
+                background:'linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)',
+                float:'right',width:'200%',
+                boxShadow:'0px 5px 10px 0px rgba(255,79,81,0.66)',
+                borderRadius:'41px',
+                border:'0px'
+              }}> 
+              <span style={{color:'#FFFFFF'}}>评论</span>
+              </Button>
+            </div>
           </Col>
         </Row>
         
@@ -47,7 +57,7 @@ export default () => (
           <Col>
             <Row>
               <Col span={3}>
-                <div style={{fontSize:'150%',marginTop: '15%'}}>
+                <div style={{fontSize:'100%',marginTop: '15%',fontWeight:500,color:'rgba(74,74,74,1)'}}>
                   热门评论
                 </div>
               </Col>
