bc1df5331bfd6343bbb0c5eda142f720c34be888
Nanqing Li <lnqjacky@gmail.com>
2018-11-14 11:16:10 +0000

    ad

--- app/ftfe/src/home/ad_title.js
+++ app/ftfe/src/home/ad_title.js
@@ -6,93 +6,265 @@ import img3 from '../image/004.jpeg'
 import img4 from '../image/005.jpg'
 import img5 from '../image/006.jpg'
 import './hot.less'
+import { Row,Col} from 'antd'
 
-const ad1=(<div style={{position:'absolute',
-            top:'135px',
-            left:'95px',
-            marginRight:'60%',
-            }}>
-            <div style={{fontWeight:600,
-            color:'rgba(255,255,255,1)',
-            fontSize:'36px',
-            maxWidth:'405px',
-            maxHeight:'50px',
-            }}>这幅广告图的正标题</div>
+
+
+const ad1=(<div>
+
+    <div style={{
+    marginLeft:190,
+    width:1250,
+    height:570,
+    backgroundRepeat:'no-repeat',
+    backgroundPosition:'50% 50%',
+    backgroundColor:'black',
+    overflow:'hidden',
+    backgroundSize:'contain',
+    backgroundImage:`url(${img1})`,
+    }}>
+    <span style={{
+    position:'absolute',
+    left:0,
+    width:190,
+    height:570,
+    background:'#0058D1',
+    }}></span>
+    <div style={{
+    width:1081,
+    height:570,
+    background:'linear-gradient(270deg,rgba(0,94,255,0) 0%,rgba(35,93,173,0.5) 28%,rgba(0,88,209,1) 100%)',
+    }}></div>
+    
+    <div style={{
+    position:'absolute',
+    top:135,
+    left:95, 
+    width:405,
+    }}>
+            <div style={{lineHeight:50, fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
             <br />
             <div style={{
-            fontWeight:600,
+            fontWeight:500,
+            lineHeight:33,
             color:'rgba(255,255,255,1)',
-            fontSize:'24px',
-            maxWidth:'405px',
-            maxHeight:'33px',
-            }}>这幅广告图的副标题</div>
+            fontSize:24}}>这幅广告图的副标题</div>
             <br />
-            <div style={{
-            fontWeight:400,
+            <div style={{fontWeight:400,
             color:'rgba(255,255,255,1)',
-            fontSize:'16px',
-            maxWidth:'405px',
-            maxHeight:'66px',
+            fontSize:16,
+            lineHeight:22,
             }}>
             同言想人別安，多學水不事少百量他策自、交施力演線成，的打小放你，竟事清保身人張著大片國相院人靜連美海證體情孩幾一。度關文出學。
             </div>
+        </div>
+    </div>
+
+    </div>)
+const ad2=(<div>
+
+    <div style={{
+    marginLeft:190,
+    width:1250,
+    height:570,
+    backgroundRepeat:'no-repeat',
+    backgroundPosition:'50% 50%',
+    backgroundColor:'black',
+    overflow:'hidden',
+    backgroundSize:'contain',
+    backgroundImage:`url(${img2})`,
+    }}>
+    <span style={{
+    position:'absolute',
+    left:0,
+    width:190,
+    height:570,
+    background:'#0058D1',
+    }}></span>
+    <div style={{
+    width:1081,
+    height:570,
+    background:'linear-gradient(270deg,rgba(0,94,255,0) 0%,rgba(35,93,173,0.5) 28%,rgba(0,88,209,1) 100%)',
+    }}></div>
+    
+    <div style={{
+    position:'absolute',
+    top:135,
+    left:95, 
+    width:405,
+    }}>
+            <div style={{lineHeight:50, fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
             <br />
+            <div style={{
+            fontWeight:500,
+            lineHeight:33,
+            color:'rgba(255,255,255,1)',
+            fontSize:24}}>这幅广告图的副标题</div>
             <br />
-            <div style={{fontSize:'16px'}}>
-                <Button style={{
-                width:150,
-                height:40,
-                background:'linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)',
-                color:'#FFFFFF',
-                borderRadius:20,
-                border:'none'
-                }}><span >播放</span></Button> 
-                <Button style={{
-                width:150,
-                height:40,
-                borderRadius:20,
-                background:'none',
-                color:'#FFFFFF',
-                marginLeft:30,
-                }}><span >收藏</span></Button> 
-            </div>
-        </div>)
-const ad2=(<div style={{position:'absolute',top:'20%',left:'10%', marginRight:'60%'}}>
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
-            <br />
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'150%'}}>这幅广告图的副标题</div>
-            <br />
-            <div style={{fontWeight:400,color:'rgba(255,255,255,1)',fontSize:'100%'}}>
+            <div style={{fontWeight:400,
+            color:'rgba(255,255,255,1)',
+            fontSize:16,
+            lineHeight:22,
+            }}>
             同言想人別安，多學水不事少百量他策自、交施力演線成，的打小放你，竟事清保身人張著大片國相院人靜連美海證體情孩幾一。度關文出學。
             </div>
-        </div>)
-const ad3=(<div style={{position:'absolute',top:'20%',left:'10%', marginRight:'60%'}}>
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
+        </div>
+    </div>
+
+    </div>)
+const ad3=(<div>
+
+    <div style={{
+    marginLeft:190,
+    width:1250,
+    height:570,
+    backgroundRepeat:'no-repeat',
+    backgroundPosition:'50% 50%',
+    backgroundColor:'black',
+    overflow:'hidden',
+    backgroundSize:'contain',
+    backgroundImage:`url(${img3})`,
+    }}>
+    <span style={{
+    position:'absolute',
+    left:0,
+    width:190,
+    height:570,
+    background:'#0058D1',
+    }}></span>
+    <div style={{
+    width:1081,
+    height:570,
+    background:'linear-gradient(270deg,rgba(0,94,255,0) 0%,rgba(35,93,173,0.5) 28%,rgba(0,88,209,1) 100%)',
+    }}></div>
+    
+    <div style={{
+    position:'absolute',
+    top:135,
+    left:95, 
+    width:405,
+    }}>
+            <div style={{lineHeight:50, fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
             <br />
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'150%'}}>这幅广告图的副标题</div>
+            <div style={{
+            fontWeight:500,
+            lineHeight:33,
+            color:'rgba(255,255,255,1)',
+            fontSize:24}}>这幅广告图的副标题</div>
             <br />
-            <div style={{fontWeight:400,color:'rgba(255,255,255,1)',fontSize:'100%'}}>
+            <div style={{fontWeight:400,
+            color:'rgba(255,255,255,1)',
+            fontSize:16,
+            lineHeight:22,
+            }}>
             同言想人別安，多學水不事少百量他策自、交施力演線成，的打小放你，竟事清保身人張著大片國相院人靜連美海證體情孩幾一。度關文出學。
             </div>
-        </div>)
-const ad4=(<div style={{position:'absolute',top:'20%',left:'10%', marginRight:'60%'}}>
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
+        </div>
+    </div>
+
+    </div>)
+const ad4=(<div>
+
+    <div style={{
+    marginLeft:190,
+    width:1250,
+    height:570,
+    backgroundRepeat:'no-repeat',
+    backgroundPosition:'50% 50%',
+    backgroundColor:'black',
+    overflow:'hidden',
+    backgroundSize:'contain',
+    backgroundImage:`url(${img4})`,
+    }}>
+    <span style={{
+    position:'absolute',
+    left:0,
+    width:190,
+    height:570,
+    background:'#0058D1',
+    }}></span>
+    <div style={{
+    width:1081,
+    height:570,
+    background:'linear-gradient(270deg,rgba(0,94,255,0) 0%,rgba(35,93,173,0.5) 28%,rgba(0,88,209,1) 100%)',
+    }}></div>
+    
+    <div style={{
+    position:'absolute',
+    top:135,
+    left:95, 
+    width:405,
+    }}>
+            <div style={{lineHeight:50, fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
             <br />
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'150%'}}>这幅广告图的副标题</div>
+            <div style={{
+            fontWeight:500,
+            lineHeight:33,
+            color:'rgba(255,255,255,1)',
+            fontSize:24}}>这幅广告图的副标题</div>
             <br />
-            <div style={{fontWeight:400,color:'rgba(255,255,255,1)',fontSize:'100%'}}>
+            <div style={{fontWeight:400,
+            color:'rgba(255,255,255,1)',
+            fontSize:16,
+            lineHeight:22,
+            }}>
             同言想人別安，多學水不事少百量他策自、交施力演線成，的打小放你，竟事清保身人張著大片國相院人靜連美海證體情孩幾一。度關文出學。
             </div>
-        </div>)
-const ad5=(<div style={{position:'absolute',top:'20%',left:'10%', marginRight:'60%'}}>
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
+        </div>
+    </div>
+
+    </div>)
+const ad5=(<div>
+
+    <div style={{
+    marginLeft:190,
+    width:1250,
+    height:570,
+    backgroundRepeat:'no-repeat',
+    backgroundPosition:'50% 50%',
+    backgroundColor:'black',
+    overflow:'hidden',
+    backgroundSize:'contain',
+    backgroundImage:`url(${img5})`,
+    }}>
+    <span style={{
+    position:'absolute',
+    left:0,
+    width:190,
+    height:570,
+    background:'#0058D1',
+    }}></span>
+    <div style={{
+    width:1081,
+    height:570,
+    background:'linear-gradient(270deg,rgba(0,94,255,0) 0%,rgba(35,93,173,0.5) 28%,rgba(0,88,209,1) 100%)',
+    }}></div>
+    
+    <div style={{
+    position:'absolute',
+    top:135,
+    left:95, 
+    width:405,
+    }}>
+            <div style={{lineHeight:50, fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'36px'}}>这幅广告图的正标题</div>
             <br />
-            <div style={{fontWeight:600,color:'rgba(255,255,255,1)',fontSize:'150%'}}>这幅广告图的副标题</div>
+            <div style={{
+            fontWeight:500,
+            lineHeight:33,
+            color:'rgba(255,255,255,1)',
+            fontSize:24}}>这幅广告图的副标题</div>
             <br />
-            <div style={{fontWeight:400,color:'rgba(255,255,255,1)',fontSize:'100%'}}>
+            <div style={{fontWeight:400,
+            color:'rgba(255,255,255,1)',
+            fontSize:16,
+            lineHeight:22,
+            }}>
             同言想人別安，多學水不事少百量他策自、交施力演線成，的打小放你，竟事清保身人張著大片國相院人靜連美海證體情孩幾一。度關文出學。
             </div>
-        </div>)
+        </div>
+    </div>
+
+    </div>)
         
 
 
@@ -102,39 +274,21 @@ export default () => (
     <div  style={{}}>
         <Carousel  effect="fade">
             <div>
+            {ad1}
             </div>
             
             
-            <div><div style={{backgroundRepeat:'no-repeat',
-            backgroundPosition:'50% 50%',
-            backgroundColor:'black',overflow:'hidden',
-            backgroundSize:'contain',height: '570px',width:'100%',
-            backgroundImage:`url(${img2})`}}>
-            </div>
+            <div>
             {ad2}
             </div>
-            <div><div style={{backgroundRepeat:'no-repeat',
-            backgroundPosition:'50% 50%',
-            backgroundColor:'black',overflow:'hidden',
-            backgroundSize:'contain',height: '570px',width:'100%',
-            backgroundImage:`url(${img3})`}}>
-            </div>
+            
+            <div>
             {ad3}
             </div>
-            <div><div style={{backgroundRepeat:'no-repeat',
-            backgroundPosition:'50% 50%',
-            backgroundColor:'black',overflow:'hidden',
-            backgroundSize:'contain',height: '570px',width:'100%',
-            backgroundImage:`url(${img4})`}}>
-            </div>
+            <div>
             {ad4}
             </div>
-            <div><div style={{backgroundRepeat:'no-repeat',
-            backgroundPosition:'50% 50%',
-            backgroundColor:'black',overflow:'hidden',
-            backgroundSize:'contain',height: '570px',width:'100%',
-            backgroundImage:`url(${img5})`}}>
-            </div>
+            <div>
             {ad5}
             </div>
         </Carousel>
