a482d64a0e5224d09d0ec94672a9ec3e36d3fcfc
Nanqing Li <lnqjacky@gmail.com>
2018-11-15 03:06:30 +0000

    icon update

--- app/ftfe/src/my_icon.js
+++ app/ftfe/src/my_icon.js
@@ -1,7 +1,7 @@
 import { Icon } from 'antd'
 
 const MyIcon = Icon.createFromIconfontCN({
-  scriptUrl: '//at.alicdn.com/t/font_863832_lw1wpnknlxb.js',
+  scriptUrl: '//at.alicdn.com/t/font_863832_m3doa1sz84r.js',
 })
 
 export default MyIcon
