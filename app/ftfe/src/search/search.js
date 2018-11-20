9ec719f81f851f98216eac03fbca4c624c973e44
Nanqing Li <lnqjacky@gmail.com>
2018-11-20 04:12:07 +0000

    mini searach moving by scroll

--- app/ftfe/src/search/search.js
+++ app/ftfe/src/search/search.js
@@ -6,27 +6,42 @@ import SmallSearch from './small_search'
 
 
 export default class Search extends Component {
-  
+  state={
+    scrollOut: false,
+    scrollThreshold: 600,
+    
+  }
+  componentDidMount () {
+    window.addEventListener('scroll', this.handleScroll.bind(this))
+  }
+  handleScroll (e) {
+    //console.log(document.documentElement.scrollTop)
+    if (!this.state.scrollOut && document.documentElement.scrollTop > this.state.scrollThreshold) {
+      this.setState(() => {return { scrollOut: true }})
+    } else if (this.state.scrollOut && document.documentElement.scrollTop < this.state.scrollThreshold) {
+      this.setState(() => {return { scrollOut: false }})
+    }
+  }
   
   render () {
     return (
-      <div style={{background:'#FEFEFE',}}>
+      <div style={{background:'#FFFFFF',}}>
         <Row>
           <Col span={24}>
             <SearchTop />
           </Col>
-          <Col span={24}>
+          {this.state.scrollOut?<Col span={24}>
             <div style={{
             position:'fixed', 
             top:'42px',
             width:'100%', height: 60,
             zIndex: 99,
             boxShadow:'0px 2px 31px 0px rgba(205,205,205,0.27)}',
-            background:'#F8F8F8',
+            background:'#FFFFFF',
             }}>
               <SmallSearch />
             </div>
-          </Col>
+          </Col>:''}
           <Col span={24}>
             <SearchList />
           </Col>
