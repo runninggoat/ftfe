0ce31e2ea6fb993d79c0c43525b07c2bf9c6c5dc
Tom郭俊杰 <1501832474@qq.com>
2018-11-22 18:13:50 +0800

    Update grid system in home page to align the tabs.

--- app/ftfe/src/home/home.js
+++ app/ftfe/src/home/home.js
@@ -20,85 +20,113 @@ class Home extends Component {
         <Col span ={24}>
           <Ad />
         </Col>
-        <Col span ={24}><div style={{
-          backgroundColor:'#FFFFFF',
-          boxShadow:'0px 2px 31px 0px rgba(205,205,205,0.27)',
-          height:54,
-        }}> 
-          <Row>
-            <Col offset={6} span ={18} ><div style={{maxWidth:1440}}>
-              <Tabs defaultActiveKey="1" size={'large'}>
-                  <TabPane tab={<span style={{margin:'0 30%'}} >推&nbsp;&nbsp;荐</span>} key="1"></TabPane>
-                  <TabPane tab={<span style={{margin:'0 30%'}} >视&nbsp;&nbsp;频</span>} key="2"></TabPane>
-                  <TabPane tab={<span style={{margin:'0 30%'}} >文&nbsp;&nbsp;学</span>} key="3"></TabPane>
-                  <TabPane tab={<span style={{margin:'0 30%'}} >其&nbsp;&nbsp;他</span>} key="4"></TabPane>
-              </Tabs></div>
-            </Col>
-          </Row>
-                
-        </div></Col>
+        <Col
+          span ={24}
+          style={{
+            backgroundColor:'#FFFFFF',
+            boxShadow:'0px 2px 31px 0px rgba(205,205,205,0.27)',
+            height:54,
+          }}
+        >
+          <div style={{
+              maxWidth:'1440px',
+              minWidth:'1200px',
+              margin: 'auto',
+            }}
+          > 
+            <Row type="flex" justify="center">
+              <Col offset={1} span ={22} >
+                <Tabs defaultActiveKey="1" size={'large'} style={{ marginLeft: '-30px' }}>
+                    <TabPane tab={<span>推&nbsp;&nbsp;荐</span>} key="1"></TabPane>
+                    <TabPane tab={<span>视&nbsp;&nbsp;频</span>} key="2"></TabPane>
+                    <TabPane tab={<span>文&nbsp;&nbsp;学</span>} key="3"></TabPane>
+                    <TabPane tab={<span>其&nbsp;&nbsp;他</span>} key="4"></TabPane>
+                </Tabs>
+              </Col>
+            </Row>
+          </div>
+        </Col>
         
-        <div
-        style={{
-          backgroundColor:'#FEFEFE',
-          maxWidth:'1440px',
-          minWidth:'1200px',
-        }}
-      >
-        
-        <Col span={24} >
-               <Carousel>
-                <div><HotMovieList /></div>
-                <div><HotMovieList /></div>
-                <div><HotMovieList /></div>
-                <div><HotMovieList /></div>
-                <div><HotMovieList /></div>
-              </Carousel>
+        <Col span={24} style={{ marginTop: '5px' }}>
+          <div style={{
+              backgroundColor:'#FEFEFE',
+              maxWidth:'1440px',
+              minWidth:'1200px',
+              margin: 'auto',
+            }}
+          >
+            <Carousel>
+              <div><HotMovieList /></div>
+              <div><HotMovieList /></div>
+              <div><HotMovieList /></div>
+              <div><HotMovieList /></div>
+              <div><HotMovieList /></div>
+            </Carousel>
+          </div>
         </Col>
         
         <Col span={24} >
-               <Carousel>
-                <div><WatchList /></div>
-                <div><WatchList /></div>
-                <div><WatchList /></div>
-                <div><WatchList /></div>
-                <div><WatchList /></div>
-              </Carousel>
+          <div style={{
+              backgroundColor:'#FEFEFE',
+              maxWidth:'1440px',
+              minWidth:'1200px',
+              margin: 'auto',
+            }}
+          >
+            <Carousel>
+              <div><WatchList /></div>
+              <div><WatchList /></div>
+              <div><WatchList /></div>
+              <div><WatchList /></div>
+              <div><WatchList /></div>
+            </Carousel>
+          </div>
         </Col>
         <Col span={24} >
-          <div><NewUpload /></div>
+          <div style={{
+              backgroundColor:'#FEFEFE',
+              maxWidth:'1440px',
+              minWidth:'1200px',
+              margin: 'auto',
+            }}
+          >
+            <div><NewUpload /></div>
+          </div>
         </Col>
         
         
         <Col span ={24}>
-          <Row>
-            <Col span={9} />
-            <Col span={6} >
-              <div style={{textAlign:'center'}}>
-                <Button style={{
-                  background:'linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)',
-                  margin:'30px 0 60px 0',
-                  width:'300px',
-                  height:'45px',
-                  boxShadow:'0px 5px 10px 0px rgba(255,79,81,0.66)',
-                  borderRadius:'41px',
-                  border:'0px'
-                }}> 
-                <MyIcon type="icon-refresh" theme="outlined" style={{color:'#FFFFFF'}} />
-                <span style={{color:'#FFFFFF'}}>加载更多</span>
-                </Button>
-              </div>
-            </Col>
-            <Col span={9} />
-          </Row>
-          
+          <div style={{
+              backgroundColor:'#FEFEFE',
+              maxWidth:'1440px',
+              minWidth:'1200px',
+              margin: 'auto',
+            }}
+          >
+            <Row>
+              <Col span={9} />
+              <Col span={6} >
+                <div style={{textAlign:'center'}}>
+                  <Button style={{
+                    background:'linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)',
+                    margin:'30px 0 60px 0',
+                    width:'300px',
+                    height:'45px',
+                    boxShadow:'0px 5px 10px 0px rgba(255,79,81,0.66)',
+                    borderRadius:'41px',
+                    border:'0px'
+                  }}> 
+                  <MyIcon type="icon-refresh" theme="outlined" style={{color:'#FFFFFF'}} />
+                  <span style={{color:'#FFFFFF'}}>加载更多</span>
+                  </Button>
+                </div>
+              </Col>
+              <Col span={9} />
+            </Row>
+          </div>
         </Col>
         <PlayModal />
-          </div>
-          
       </Row>
-      
-      
     )
   }
 }
