2c2f4b8bb63efa91db3ff4528682c3c58f12b967
shadowstream <nanqingl@qq.com>
2018-11-27 12:33:09 +0800

    funding main page

--- app/ftfe/src/funding/index.js
+++ app/ftfe/src/funding/index.js
@@ -1,21 +1,28 @@
 import React, { Component } from "react";
 import Ad from "../home/ad_title";
-import { Row, Col, Button, Icon, Carousel } from "antd";
+import { Row, Col, Pagination } from "antd";
 import PlayModal from "../modal/play_modal";
-import Title from './title'
+import Title from "./title";
 import MyIcon from "../my_icon";
 import { Tabs } from "antd";
 import "../home/hot.less";
 import img1 from "../image/001.jpeg";
-import img2 from "../image/003.jpg";
+import img2 from "../image/007.jpg";
 import img3 from "../image/004.jpeg";
 import img4 from "../image/005.jpg";
 import img5 from "../image/006.jpg";
 import img9 from "../image/009.jpg";
 
 const TabPane = Tabs.TabPane;
-const color1='linear-gradient(270deg,rgba(250,217,97,1) 0%,rgba(249,163,63,1) 100%)'
-
+const color1 =
+  "linear-gradient(270deg,rgba(250,217,97,1) 0%,rgba(249,163,63,1) 100%)";
+const fontcolor1 = "#FAAD14";
+const color2 =
+  "linear-gradient(90deg,rgba(255,132,135,1) 0%,rgba(255,19,103,1) 100%)";
+const fontcolor2 = "#FF1367";
+const color3 =
+  "linear-gradient(270deg,rgba(197,222,49,1) 0%,rgba(67,245,219,1) 100%)";
+const fontcolor3 = "#52C41A";
 class Home extends Component {
   render() {
     return (
@@ -51,17 +58,47 @@ class Home extends Component {
                   <TabPane tab={<span>成功项目</span>} key="4" />
                 </Tabs>
               </Col>
-              
             </Row>
-            
-            
           </div>
         </Col>
-        <Col span={24} style={{
-            margin:'50px 0 0',}}>
-            <div >
-                <Title img={img1} background={color1}/>
+        <Col
+          span={24}
+          style={{
+            margin: "50px 0 0"
+          }}
+        >
+          <div
+            style={{
+              maxWidth: "1440px",
+              minWidth: "1200px",
+              margin: "auto"
+            }}
+          >
+            <Title
+              img={img1}
+              color={color1}
+              fontcolor={fontcolor1}
+              percent={25}
+              money={"16,000"}
+            />
+            <Title
+              img={img2}
+              color={color2}
+              percent={0}
+              fontcolor={fontcolor2}
+              money={0}
+            />
+            <Title
+              img={img3}
+              fontcolor={fontcolor3}
+              color={color3}
+              percent={100}
+              money={"20,000"}
+            />
+            <div style={{margin: '48px 0px 80px 900px',}}>
+              <Pagination defaultCurrent={2} total={200} />
             </div>
+          </div>
         </Col>
 
         <PlayModal />
---
+++ app/ftfe/src/funding/title_right.js
@@ -0,0 +1,206 @@
+import React from "react";
+import { Row, Col, Button,} from "antd";
+import { Divider } from 'antd';
+
+import { Tooltip, Progress } from 'antd';
+import { Tag } from 'antd';
+
+
+const Right =(props) =>{
+    return(
+        <Row >
+            <Col span={24}>
+                <div style={{
+                    fontSize:26,
+                    fontWeight:500,
+                    color:'rgba(0,0,0,1)',
+                    lineHeight:'37px',
+                    margin:'28px 0 14px',
+                }}> 
+                    这个项目的标题这个项目的标题
+                </div>
+            </Col>
+            <Col span={24}>
+            <Tag style={{
+                    margin:"0 5px",
+                    color:"#9B9B9B",
+                    width:55,
+                    height:20,
+                    background:'rgba(238,238,238,1)',
+                    borderRadius:'10px',
+                    textAlign: 'center',
+                }}>动画</Tag>
+                <Tag style={{
+                    margin:"0 5px",
+                    color:"#9B9B9B",
+                    width:55,
+                    height:20,
+                    background:'rgba(238,238,238,1)',
+                    borderRadius:'10px',
+                    textAlign: 'center',
+                }}>视频</Tag>
+                <Tag style={{
+                    margin:"0 5px",
+                    color:"#9B9B9B",
+                    width:55,
+                    height:20,
+                    background:'rgba(238,238,238,1)',
+                    borderRadius:'10px',
+                    textAlign: 'center',
+                }}>原创</Tag>
+            </Col>
+            <Col span={4}>
+                {props.percent==0?(<div style={{
+                    margin:'23px 0 26px',
+                    fontSize:'25px',
+                    fontWeight:500,
+                    color:props.fontcolor,
+                    lineHeight:'40px',
+                }}>预热中</div>):(
+                    props.percent==100?(
+                        <div style={{
+                            margin:'23px 0 26px',
+                            fontSize:'25px',
+                            fontWeight:500,
+                            color:props.fontcolor,
+                            lineHeight:'40px',
+                        }}>项目成功</div>
+                    ):(
+                        <div style={{
+                            margin:'23px 0 26px',
+                            fontSize:'40px',
+                            fontWeight:500,
+                            color:'#FAAD14',
+                            lineHeight:'40px',
+                        }}>{props.percent}<span style={{fontSize:'24px',}}>%</span></div>
+                    )
+                )}
+
+                
+            </Col>
+            <Col span={14}>
+                <div style={{
+                    margin:'35px 0 26px',
+                }}>
+                    <Progress 
+                    percent={props.percent} 
+                    status="active"
+                    strokeColor={props.color}
+                    showInfo={false}
+                    />
+                </div> 
+            </Col>
+            <Col span={24}>
+                <Row >
+                    <Col span={4}>
+                    <div style={{
+                    color:'#FF1367',
+                    fontSize:'30px',
+                    fontWeight:400,
+                    lineHeight:'28px',
+                    }}>
+                        20,000 
+                    </div> 
+                    <div style={{
+                        color:'#9B9B9B',
+                        fontSize:'14px',
+                        fontWeight:400,
+                        lineHeight:'20px',
+                    }}>
+                        目标资金 (糖分)
+                    </div>
+                    </Col>
+                    <Col span={1}>
+                    <Divider type="vertical" 
+                        style={{width:'1px',
+                            height:'40px',
+                            background:'rgba(216,216,216,1)',}}
+                    />
+
+                    </Col>
+                    
+                    <Col  span={4}>
+                    <div style={{
+                    color: props.money==0?'#9B9B9B':props.fontcolor,
+                    fontSize:'30px',
+                    fontWeight:400,
+                    lineHeight:'28px',
+                    textAlign:'center',
+                    }}>
+                        {props.money}  
+                    </div> 
+                    <div style={{
+                        color:'#9B9B9B',
+                        fontSize:'14px',
+                        fontWeight:400,
+                        lineHeight:'20px',
+                        textAlign:'center',
+                    }}>
+                        已募集资金 (糖分)
+                    </div>
+                    </Col>
+                    <Col span={1}>
+                    <Divider type="vertical" 
+                        style={{width:'1px',
+                            height:'40px',
+                            background:'rgba(216,216,216,1)',}}
+                    /></Col>
+                    <Col  span={5}>
+                    <div style={{
+                    color:'#9B9B9B',
+                    fontSize:'30px',
+                    fontWeight:400,
+                    lineHeight:'28px',
+                    textAlign:'center',
+                    }}>
+                        2018.09.11  
+                    </div> 
+                    <div style={{
+                        color:'#9B9B9B',
+                        fontSize:'14px',
+                        fontWeight:400,
+                        lineHeight:'20px',
+                        textAlign:'center',
+                    }}>
+                        开始日期
+                    </div>
+                    </Col>
+                    <Col span={1}>
+                    <Divider type="vertical" 
+                        style={{width:'1px',
+                            height:'40px',
+                            background:'rgba(216,216,216,1)',}}
+                    />
+                    </Col>
+                    <Col span={6}>
+                    <div style={{
+                    color:'#9B9B9B',
+                    fontSize:'30px',
+                    fontWeight:400,
+                    lineHeight:'28px',
+                    textAlign:'center',
+                    }}>
+                        2018.09.20  
+                    </div> 
+                    <div style={{
+                        color:'#9B9B9B',
+                        fontSize:'14px',
+                        fontWeight:400,
+                        lineHeight:'20px',
+                        textAlign:'center',
+                    }}>
+                        截止日期
+                    </div>
+                    </Col>
+
+                </Row>
+                
+            </Col>
+            
+
+        </Row>
+    )
+}
+ 
+export default Right;
+                
