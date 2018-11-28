c18e3250f86b6499a49e42cbf9cd47815fb1e5dd
shadowstream <nanqingl@qq.com>
2018-11-28 15:59:56 +0800

    动态

--- app/ftfe/src/modal/fund/project_details.js
+++ app/ftfe/src/modal/fund/project_details.js
@@ -3,6 +3,11 @@ import React from "react";
 export default props => (
   <div
     style={{
+      borderTop: "1px solid #EEEEEE"
+    }}
+  >
+  <div
+    style={{
       margin: "0 33px 0 36px",
       borderTop: "1px solid #EEEEEE"
     }}
@@ -29,7 +34,7 @@ export default props => (
       &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;随着社会的发展,现代制糖工艺早已经实现了机械化,懂得民间传统土法熬糖技艺的人越来越少,土法熬糖正渐行渐远。这个时代有太多速成的东西,他们以轰轰烈烈的姿态冲击着人们的视觉和味蕾,但遗留的气息也转瞬即逝。岩山奶奶这样熬糖的过程虽然慢,但好的红糖就是要花时间慢慢熬制,才能熬出品质啊。而这份甜蜜,也需要我们慢慢地品尝,品出它最淳真的味道!
     </div>
     <div style={{ textAlign: "center", margin: "15px 0 15px" }}>
-      <img alt='' src={props.img} style={{ width: 800, height: 471 }} />
+      <img alt="" src={props.img} style={{ width: 800, height: 471 }} />
     </div>
     <div
       style={{
@@ -63,7 +68,8 @@ export default props => (
       &nbsp;&nbsp;&nbsp;&nbsp;随着社会的发展,现代制糖工艺早已经实现了机械化,懂得民间传统土法熬糖技艺的人越来越少,土法熬糖正渐行渐远。这个时代有太多速成的东西,他们以轰轰烈烈的姿态冲击着人们的视觉和味蕾,但遗留的气息也转瞬即逝。岩山奶奶这样熬糖的过程虽然慢,但好的红糖就是要花时间慢慢熬制,才能熬出品质啊。而这份甜蜜,也需要我们慢慢地品尝,品出它最淳真的味道!
     </div>
     <div style={{ textAlign: "center", margin: "15px 0 15px" }}>
-      <img alt='' src={props.img} style={{ width: 800, height: 471 }} />
+      <img alt="" src={props.img} style={{ width: 800, height: 471 }} />
     </div>
+  </div>
   </div>
 );
--- app/ftfe/src/modal/fund/timeline.js
+++ app/ftfe/src/modal/fund/timeline.js
@@ -1,29 +1,73 @@
 import React, { Component } from "react";
 import { Timeline } from "antd";
-import faker from 'faker'
+import faker from "faker";
 const text =
   "许多小伙伴会有疑问，古法红糖的食品安全问题。红糖由制糖人手工熬制、手工切割，虽不用机器加工，但也能保证生态健康。我们将红糖做了质检，可以放心食用。我们希望把原生态的原汁糖，带给更多的人。 许多小伙伴会有疑问，古法红糖的食品安全问题。红糖由制糖人手工熬制、手工切割。";
 export default props => (
-  <div style={{ margin: "35px 0 0 107px" }}>
+  <div style={{ borderTop: "1px solid #EEEEEE" }}>
+  <div style={{ margin: "35px 44px 30px 107px"}}>
     <Timeline>
       <Timeline.Item
-        color="#4A4A4A"
+        color="#FF1367"
         style={{ fontSize: 15, fontseight: 400, lineHeight: "21px" }}
       >
         {text}
         <div>
-          <img src={faker.image.food()} />
+          <img
+            src={faker.image.food()}
+            style={{ height: 90, width: 90, marginRight: "10px" }}
+          />
+          <img
+            src={faker.image.sports()}
+            style={{ height: 90, width: 90, marginRight: "10px" }}
+          />
+        </div>
+        <div>
+          <img
+            src={faker.image.nightlife()}
+            style={{ height: 90, width: 90, marginRight: "10px" }}
+          />
         </div>
       </Timeline.Item>
-      <Timeline.Item color="#4A4A4A">
-        Solve initial network problems 2015-09-01
-      </Timeline.Item>
-      <Timeline.Item color="#4A4A4A">
-        >Technical testing 2015-09-01
-      </Timeline.Item>
-      <Timeline.Item color="#4A4A4A">
-        Network problems being solved 2015-09-01
-      </Timeline.Item>
+      <Timeline.Item color="#FF1367">{text}</Timeline.Item>
+      <Timeline.Item color="#FF1367">{text}</Timeline.Item>
+      <Timeline.Item color="#FF1367">{text}</Timeline.Item>
     </Timeline>
-  </div>
+    <div
+      style={{
+        position: "absolute",
+        top: "80px",
+        left: "45px"
+      }}
+    >
+      2小时前
+    </div>
+    <div
+      style={{
+        position: "absolute",
+        top: "345px",
+        left: "45px"
+      }}
+    >
+      1天前
+    </div>
+    <div
+      style={{
+        position: "absolute",
+        top: "428px",
+        left: "45px"
+      }}
+    >
+      59天前
+    </div>
+    <div
+      style={{
+        position: "absolute",
+        top: "510px",
+        left: "18px"
+      }}
+    >
+      2018-09-28
+    </div>
+  </div></div>
 );
